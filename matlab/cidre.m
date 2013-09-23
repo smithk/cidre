13d0bfe70afe37463a1b653714f2ddfbeba191a4
smithk <cheeseburgerholdthecheese@gmail.com>
2013-09-23 18:50:33 +0200

    Added cidre.m to Matlab project.
    Signed-off-by: smithk <cheeseburgerholdthecheese@gmail.com>

---
+++ matlab/cidre.m
@@ -0,0 +1,24 @@
+function cidre(varargin)
+% A general illumination correction method for optical microscopy. Should
+% be applied to collections of images.
+%
+% USAGE:
+%
+% OUTPUT:
+%  
+% See also: cidreGui, cidreCorrect, cidreModel,
+
+% From the CIDRE project, a general illumination correction method for
+% optical microscopy (https://github.com/smithk/cidre).
+% Copyright © 2013 Kevin Smith and Peter Horvath, Light Microscopy and 
+% Screening Centre (LMSC), Swiss Federal Institute of Technology Zurich 
+% (ETHZ), Switzerland. All rights reserved.
+%
+% This program is free software; you can redistribute it and/or modify it 
+% under the terms of the GNU General Public License version 2 (or higher) 
+% as published by the Free Software Foundation. This program is 
+% distributed WITHOUT ANY WARRANTY; without even the implied warranty of 
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+% General Public License for more details.
+
+addpath 3rdparty/ gui/ io/ main/
