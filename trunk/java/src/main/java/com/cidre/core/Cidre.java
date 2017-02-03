48a2e1ec0e12a73485b26a2ab6b5df190cc43472
Emil Rozbicki <emilroz@gmail.com>
2017-02-03 16:41:24 +0100

    BuildModel

--- trunk/java/src/main/java/com/cidre/core/Cidre.java
+++ trunk/java/src/main/java/com/cidre/core/Cidre.java
@@ -2,4 +2,44 @@ package com.cidre.core;
 
 public class Cidre {
 
+    private static final long serialVersionUID = 1L;
+
+    private final int lambdaRScaleFactor = 10;
+
+    private final int lambdaRMinValue = 0;
+
+    private final int lambdaRMaxValue = 9;
+
+    private final double lambdaRDefaultValue = 6;
+
+    private final int lambdaZScaleFactor = 10;
+
+    private final int lambdaZMinValue = -2;
+
+    private final int lambdaZMaxValue = 5;
+
+    private final double lambdaZDefaultValue = 0.5;
+
+    public Cidre() {};
+
+    /*
+     * Build CIDRE model
+     */
+    private void buildModel(
+            Double zMin, Double zMax, Double lambdaVreg, Double lambdaZero)
+    {
+        Options options = new Options();
+        options.folderSource = "";
+        options.fileFilterSource = "";
+        options.folderDestination = "";
+        options.lambdaVreg = lambdaVreg;
+        options.lambdaZero = lambdaZero;
+        options.zLimits[0] = zMin;
+        options.zLimits[1] = zMax;
+    }
+
+    public void run() {};
+
+    public void applyCorrection() {};
+
 }
