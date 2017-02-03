64c10baaae43537fa870d2a79c06707a784940da
Emil Rozbicki <emilroz@gmail.com>
2017-02-03 16:42:03 +0100

    Options

--- trunk/java/src/main/java/com/cidre/core/Options.java
+++ trunk/java/src/main/java/com/cidre/core/Options.java
@@ -1,5 +1,31 @@
 package com.cidre.core;
 
-public class Options {
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Options
+{
+    public enum CorrectionMode {
+        ZERO_LIGHT_PRESERVED,
+        DYNAMIC_RANGE_CORRECTED,
+        DIRECT
+    };
+
+    public Double lambdaVreg = null;
+    public Double lambdaZero = null;
+    public Integer maxLbgfsIterations = null;
+    public Double qPercent = null;
+    public Double[] zLimits = new Double[2];
+    public Dimension imageSize;
+    public String folderSource;
+    public String fileFilterSource;
+    public String folderDestination;
+    public List<String> fileNames = new ArrayList<String>();
+    public int numImagesProvided;
+    public Integer bitDepth = null;
+    public CorrectionMode correctionMode = null;
+    public int targetNumPixels = 9400;
+    public Dimension workingSize;
+    public int numberOfQuantiles = 200;
 }
