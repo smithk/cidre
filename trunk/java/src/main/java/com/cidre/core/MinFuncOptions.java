e12ebee37d409935df71eb18b73a93cd55d6a44c
Emil Rozbicki <emilroz@gmail.com>
2017-02-03 16:55:16 +0100

    MinFuncOptions

--- trunk/java/src/main/java/com/cidre/core/MinFuncOptions.java
+++ trunk/java/src/main/java/com/cidre/core/MinFuncOptions.java
@@ -2,4 +2,28 @@ package com.cidre.core;
 
 public class MinFuncOptions {
 
+    /**
+     *  max iterations for optimization
+     */
+    public int maxIter;
+
+    /**
+     *  max evaluations of objective function
+     */
+    public int MaxFunEvals;
+
+    /**
+     *  progress tolerance
+     */
+    public double progTol;
+
+    /**
+     *  optimality tolerance
+     */
+    public double optTol;
+
+    /**
+     *  number of corrections to store in memory
+     */
+    public int Corr;
 }
