474ace426abf022b1c0b44ab5388ac6953e3c953
Emil Rozbicki <emilroz@gmail.com>
2017-02-06 14:45:17 +0100

    LbfgsAddResult

--- trunk/java/src/main/java/com/cidre/core/LbfgsAddResult.java
+++ trunk/java/src/main/java/com/cidre/core/LbfgsAddResult.java
@@ -1,5 +1,11 @@
 package com.cidre.core;
 
 public class LbfgsAddResult {
-
+    public double[][] S;
+    public double[][] Y;
+    public double[] YS;
+    public int lbfgs_start;
+    public int lbfgs_end;
+    public double Hdiag;
+    public boolean skipped;  
 }
