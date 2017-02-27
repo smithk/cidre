b31d9e2c753d247749f3acac534ec1f9dadedd39
Emil Rozbicki <emilroz@gmail.com>
2017-02-27 22:56:09 +0100

    Writer test

--- trunk/java/src/main/java/com/cidre/Main.java
+++ trunk/java/src/main/java/com/cidre/Main.java
@@ -8,13 +8,19 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.cidre.core.ImageCorrection;
 import com.cidre.core.ModelDescriptor;
 import com.cidre.core.ModelGenerator;
 import com.cidre.core.Options;
 import com.cidre.input.BfImageLoader;
 
 import ch.qos.logback.classic.Level;
-
+import loci.common.services.ServiceFactory;
+import loci.formats.FormatTools;
+import loci.formats.MetadataTools;
+import loci.formats.meta.IMetadata;
+import loci.formats.out.TiffWriter;
+import loci.formats.services.OMEXMLService;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.annotation.Arg;
 import net.sourceforge.argparse4j.impl.Arguments;
@@ -71,7 +77,7 @@ public class Main {
         main.correctImages();
     }
 
-    public void correctImages() {
+    public void correctImages() throws Exception {
      // Setup logger
         ch.qos.logback.classic.Logger root =
             (ch.qos.logback.classic.Logger)LoggerFactory.getLogger(
@@ -96,7 +102,36 @@ public class Main {
         }
         ModelGenerator model = new ModelGenerator(image_loader.getOptions());
         ModelDescriptor descriptor = model.generate(image_loader.getStack());
+        ImageCorrection corrector = new ImageCorrection(
+                descriptor, Options.CorrectionMode.ZERO_LIGHT_PRESERVED,
+                this.output, image_loader);
         log.info("Image size: {}}", descriptor.imageSize);
         log.info("Image size small: {}", descriptor.imageSize_small);
+        double[][] pixels;
+        float[][] pixelsFloat;
+        int channel = 0;
+        int zPlane = 0;
+        int timepoint = 0;
+        String imageName = output + '/';
+        String fileName;
+        ServiceFactory factory = new ServiceFactory();
+        OMEXMLService service = factory.getInstance(OMEXMLService.class);
+        IMetadata meta = service.createOMEXMLMetadata();
+        MetadataTools.populateMetadata(
+            meta, 0, null, false, "XYZCT",
+            FormatTools.getPixelTypeString(image_loader.getPixelType()),
+            image_loader.getTileSizeX(), image_loader.getTileSizeY(),
+            1, 1, 1, 1);
+        meta.setPixelsBigEndian(false, 0);
+        for (int i = 0; i < 100; i++) {
+            pixels = image_loader.loadPlane(i, channel, timepoint, zPlane);
+            pixelsFloat = corrector.correctPlane(pixels);
+            TiffWriter writer = new TiffWriter();
+            fileName = imageName + String.format("%03d", i) + ".tif";
+            writer.setId(fileName);
+            writer.saveBytes(0, pixelsFloat);
+            writer.close();
+        }
+        
     };
 }
