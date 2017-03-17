8c6f6f1a1040cce425df0b18185b527212893cc7
Emil Rozbicki <emil@glencoesoftware.com>
2017-03-17 17:15:46 +0100

    Some test + debug

--- trunk/java/src/main/java/com/cidre/Main.java
+++ trunk/java/src/main/java/com/cidre/Main.java
@@ -1,24 +1,30 @@
 package com.cidre;
 
 
+import java.awt.Dimension;
 import java.io.File;
+import java.io.FilenameFilter;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.cidre.core.CidreMath;
 import com.cidre.core.ImageCorrection;
 import com.cidre.core.ModelDescriptor;
 import com.cidre.core.ModelGenerator;
 import com.cidre.core.Options;
 import com.cidre.input.BfImageLoader;
+import com.cidre.input.BfImageWriter;
 
 import ch.qos.logback.classic.Level;
 import loci.common.services.ServiceFactory;
 import loci.formats.FormatTools;
+import loci.formats.ImageReader;
 import loci.formats.MetadataTools;
 import loci.formats.meta.IMetadata;
 import loci.formats.out.TiffWriter;
@@ -79,6 +85,126 @@ public class Main {
         main.correctImages();
     }
 
+    protected class ImageNameFilter implements FilenameFilter {
+
+        private Pattern pattern;
+
+        public ImageNameFilter(String expression) {
+            String correctedExpression = ".*";
+            if (expression != null && expression != "") {
+                correctedExpression = expression.replace(".", "\\.");
+                correctedExpression = correctedExpression.replace("*", ".*");
+            }
+            pattern = Pattern.compile(
+                correctedExpression, Pattern.CASE_INSENSITIVE);
+        }
+
+        @Override
+        public boolean accept(File directory, String fileName) {
+            return pattern.matcher(new File(fileName).getName()).matches();
+        }
+    }
+
+    private ArrayList<String> getFileList(String inputDir) {
+        ArrayList<String> fileList = new ArrayList<String>();
+        File file = new File(inputDir);
+        String path, fileName;
+        if (file.isDirectory()) {
+            path = file.toString() + File.separator;
+            fileName = "*";
+        } else {
+            path = file.getParent() + File.separator;
+            fileName = file.getName();
+        }
+        File directory = new File(path);
+        log.debug("Searching for files in {}", directory.toString());
+        File[] listOfFiles = directory.listFiles(
+            new ImageNameFilter(fileName));
+        if (listOfFiles != null) {
+            log.debug("{}", listOfFiles.toString());
+            for (int i = 0; i < listOfFiles.length; i++) {
+                if (listOfFiles[i].getName().startsWith(".")) {
+                    continue;
+                }
+                if (listOfFiles[i].isDirectory()) {
+                    continue;
+                }
+                fileList.add(path + listOfFiles[i].getName());
+                log.info("Adding {} to input",
+                          path + listOfFiles[i].getName());
+            }
+        }
+        log.info("Processing {} files", fileList.size());
+        log.debug("File list: {}", fileList);
+        return fileList;
+    }
+
+    public List<double[][]> loadPreProcessedImages(String inputDir) {
+        ArrayList<String> fileNames = this.getFileList(inputDir);
+        List<double[][]> stack = new ArrayList<double [][]>();
+        boolean fp = false;
+        boolean unsigned = false;
+        for (String fileName : fileNames) {
+            log.info("Reading from {}", fileName);
+            ImageReader reader = new ImageReader();
+            try {
+                BfImageLoader.initializeReader(reader);
+                reader.setId(fileName);
+                if (reader.getPixelType() == FormatTools.FLOAT ||
+                    reader.getPixelType() == FormatTools.DOUBLE)
+                {
+                    fp = true;
+                }
+                if (reader.getPixelType() == FormatTools.UINT8 ||
+                    reader.getPixelType() == FormatTools.UINT16 ||
+                    reader.getPixelType() == FormatTools.UINT32)
+                {
+                    unsigned = true;
+                }
+                byte[] plane = reader.openBytes(0);
+                double[][] planeDouble = BfImageLoader.toDoubleArray(
+                    plane, (int) (0.125 * reader.getBitsPerPixel()),
+                    fp, reader.isLittleEndian(), unsigned,
+                    reader.getSizeX(), reader.getSizeY());
+                stack.add(planeDouble);
+            } catch (Exception ex) {
+                log.error("{}", ex.getCause());
+            }
+        }
+        return stack;
+    }
+
+    public void setOptionsToTestValues(Options options) {
+        options.zLimits[0] = 256.0;
+        options.zLimits[1] = 379.0;
+        options.numberOfQuantiles = 221;
+        options.bitDepth = 65536;
+        options.workingSize = new Dimension(106, 89);
+        options.imageSize = new Dimension(1280, 1080);
+    }
+
+    public void testBuildModel(String inputDir) {
+        List<double[][]> stack = this.loadPreProcessedImages(inputDir);
+        Options options = new Options();
+        this.setOptionsToTestValues(options);
+        ModelGenerator model = new ModelGenerator(options);
+        ModelDescriptor descriptor = model.generate(stack);
+    }
+
+    public void printOptions(Options options) {
+        log.info(
+            "CidreOptions:\n\tlambdaVreg: {}\n\tlambdaZero: {}" +
+            "\n\tmaxLbgfsIterations: {}\n\tqPercent: {}\n\tzLimits:{}, {}" +
+            "\n\timageSize: {}\n\tnumImagesProvided: {}\n\tbitDepth: {}" +
+            "\n\tcorrectionMode: {}\n\ttargetNumPixels: {}" +
+            "\n\tworkingSize: {}\n\tnumberOfQuantiles: {}",
+            options.lambdaVreg, options.lambdaZero, options.maxLbgfsIterations,
+            options.qPercent, options.zLimits[0], options.zLimits[1],
+            options.imageSize, options.numImagesProvided, options.bitDepth,
+            options.correctionMode, options.targetNumPixels,
+            options.workingSize, options.numberOfQuantiles);
+    }
+
     public void correctImages() throws Exception {
      // Setup logger
         ch.qos.logback.classic.Logger root =
@@ -89,11 +215,16 @@ public class Main {
         } else {
             root.setLevel(Level.INFO);
         }
+        String dir = output + File.separator +  "image_loader_output";
+        //this.testBuildModel(dir);
         Options options = new Options();
         List<Integer> series = new ArrayList<Integer>();
         for (int i = 0; i < 221; i++) {
             series.add(i);
         }
+        options.zLimits[0] = 256.0;
+        options.zLimits[1] = 379.0;
+        options.numberOfQuantiles = 221;
         BfImageLoader image_loader = new BfImageLoader(
             options, this.input.get(0), series, 0, 0, 0);
         try {
@@ -102,10 +233,23 @@ public class Main {
             log.error(e.toString());
             e.printStackTrace();
         }
+        this.printOptions(options);
+        List<double[][]> stack = image_loader.getStack();
+        String imageName = output + File.separator + "File_";
+        String fileName;
         options = image_loader.getOptions();
-        options.zLimits[0] = 256.0;
-        options.zLimits[1] = 379.0;
-        options.numberOfQuantiles = 221;
+        for (int i = 0; i < stack.size(); i++) {
+            fileName = output + File.separator +  "image_loader_output"
+                     + File.separator + "img_loader_"
+                     + String.format("%03d", i) + ".tif";
+            BfImageWriter writer = new BfImageWriter(
+                fileName, options.workingSize.width,
+                options.workingSize.height,
+                FormatTools.getPixelTypeString(FormatTools.DOUBLE));
+            writer.initialise();
+            writer.write(stack.get(i), 0);
+            writer.close();
+        }
         ModelGenerator model = new ModelGenerator(options);
         ModelDescriptor descriptor = model.generate(image_loader.getStack());
         ServiceFactory factory = new ServiceFactory();
@@ -114,8 +258,7 @@ public class Main {
         int width = image_loader.getWidth();
         int height = image_loader.getHeight();
         log.info("Write image size [{}, {}]", width, height);
-        String imageName = output + File.separator + "File_";
-        String fileName;
+
         MetadataTools.populateMetadata(
             meta, 0, null, false, "XYZCT",
             FormatTools.getPixelTypeString(FormatTools.DOUBLE),
@@ -146,8 +289,6 @@ public class Main {
         }
         writerb.saveBytes(0, buffer2.array());
         writerb.close();
-        if (true)
-            return;
         ImageCorrection corrector = new ImageCorrection(
                 descriptor, Options.CorrectionMode.DYNAMIC_RANGE_CORRECTED,
                 this.output, image_loader);
@@ -158,6 +299,26 @@ public class Main {
         int channel = 0;
         int zPlane = 0;
         int timepoint = 0;
+
+        fileName = imageName + "min_image" + ".tif";
+        MetadataTools.populateMetadata(
+                meta, 0, null, false, "XYZCT",
+                FormatTools.getPixelTypeString(FormatTools.DOUBLE),
+                width, height,
+                1, 1, 1, 1);
+        TiffWriter writerMin = new TiffWriter();
+        writerMin.setMetadataRetrieve(meta);
+        writerMin.setId(fileName);
+        ByteBuffer bufferMin = ByteBuffer.allocate(8 * width * height);
+        double[][] minImage = image_loader.getMinImage();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                bufferMin.putDouble(minImage[x][y]);
+            }
+        }
+        writerMin.saveBytes(0, bufferMin.array());
+        writerMin.close();
+
         MetadataTools.populateMetadata(
                 meta, 0, null, false, "XYZCT",
                 FormatTools.getPixelTypeString(FormatTools.FLOAT),
