162639449a6675cfd479a0f28a31ed78e5681234
Emil Rozbicki <emil@glencoesoftware.com>
2017-03-28 11:15:15 +0200

    Cidre driver

--- trunk/java/src/main/java/com/cidre/core/Cidre.java
+++ trunk/java/src/main/java/com/cidre/core/Cidre.java
@@ -1,6 +1,26 @@
 package com.cidre.core;
 
+import java.io.File;
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.cidre.algorithms.CidreMath;
+import com.cidre.io.BfImageLoader;
+import com.cidre.preprocessing.CidrePreprocess;
+
+import loci.common.services.ServiceFactory;
+import loci.formats.FormatTools;
+import loci.formats.MetadataTools;
+import loci.formats.meta.IMetadata;
+import loci.formats.out.TiffWriter;
+import loci.formats.services.OMEXMLService;
+
 public class Cidre {
+
+    private static final Logger log =
+        LoggerFactory.getLogger(Cidre.class);
 
     private static final long serialVersionUID = 1L;
 
@@ -20,15 +40,44 @@ public class Cidre {
 
     private final double lambdaZDefaultValue = 0.5;
 
-    public Cidre() {};
+    private String input;
 
-    /*
+    private String outputDir;
+
+    private int width;
+
+    private int height;
+
+    public Cidre(String fileName, String outputDir) {
+        this.input = fileName;
+        this.outputDir = outputDir;
+    };
+
+    /**
      * Build CIDRE model
      */
-    private void buildModel(
-            Double zMin, Double zMax, Double lambdaVreg, Double lambdaZero)
+    public void buildModel()
     {
         Options options = new Options();
+        
+        BfImageLoader image_loader = new BfImageLoader(
+            options, this.input, series, 0, 0);
+        try {
+            image_loader.loadImages(1);
+        } catch (Exception e) {
+            log.error(e.toString());
+            e.printStackTrace();
+        }
+        double[] zLimits = CidreMath.zLimitsFromPercentiles(
+            image_loader.getMinImage());
+        options.zLimits[0] = zLimits[0];
+        options.zLimits[1] = zLimits[1];
+        this.printOptions(options);
+
+        ModelGenerator model = new ModelGenerator(options);
+        ModelDescriptor descriptor = model.generate(
+            image_loader.getStack());
+        /*
         options.folderSource = "";
         options.fileFilterSource = "";
         options.folderDestination = "";
@@ -36,10 +85,74 @@ public class Cidre {
         options.lambdaZero = lambdaZero;
         options.zLimits[0] = zMin;
         options.zLimits[1] = zMax;
+        */
     }
 
-    public void run() {};
+    /**
+     * Load CIDRE model
+     */
+    public void loadModel() {};
 
-    public void applyCorrection() {};
+    /**
+     * Save CIDRE model
+     */
+    public void saveModel(ModelDescriptor descriptor) throws Exception
+    {
+        // Save Model V
+        ServiceFactory factory = new ServiceFactory();
+        OMEXMLService service = factory.getInstance(OMEXMLService.class);
+        IMetadata meta = service.createOMEXMLMetadata();
+        MetadataTools.populateMetadata(
+            meta, 0, null, false, "XYZCT",
+            FormatTools.getPixelTypeString(FormatTools.DOUBLE),
+            this.width, this.height, 1, 1, 1, 1);
+        TiffWriter writer = new TiffWriter();
+        writer.setMetadataRetrieve(meta);
+        String fileName = this.outputDir + File.separator
+                        + "Model_V" + ".tif";
+        writer.setId(fileName);
+        ByteBuffer buffer = ByteBuffer.allocate(8 * this.width * this.height);
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++) {
+                buffer.putDouble(descriptor.v[x * this.height + y]);
+            }
+        }
+        writer.saveBytes(0, buffer.array());
+        writer.close();
+        // Save model Z
+        writer = new TiffWriter();
+        writer.setMetadataRetrieve(meta);
+        fileName = this.outputDir + File.separator
+                 + "Model_Z" + ".tif";
+        writer.setId(fileName);
+        buffer = ByteBuffer.allocate(8 * this.width * this.height);
+        for (int y = 0; y < this.height; y++) {
+            for (int x = 0; x < this.width; x++) {
+                buffer.putDouble(descriptor.z[x * this.height + y]);
+            }
+        }
+        writer.saveBytes(0, buffer.array());
+        writer.close();
+    };
+
+    /**
+     * Apply model
+     */
+    public void applyModel() {};
+
+    private void printOptions(Options options){
+        log.info(
+            "CidreOptions:\n\tlambdaVreg: {}\n\tlambdaZero: {}" +
+            "\n\tmaxLbgfsIterations: {}\n\tqPercent: {}\n\tzLimits:{}, {}" +
+            "\n\timageSize: {}\n\tnumImagesProvided: {}\n\tbitDepth: {}" +
+            "\n\tcorrectionMode: {}\n\ttargetNumPixels: {}" +
+            "\n\tworkingSize: {}\n\tnumberOfQuantiles: {}",
+            options.lambdaVreg, options.lambdaZero, options.maxLbgfsIterations,
+            options.qPercent, options.zLimits[0], options.zLimits[1],
+            options.imageSize, options.numImagesProvided, options.bitDepth,
+            options.correctionMode, options.targetNumPixels,
+            options.workingSize, options.numberOfQuantiles
+        );
+    }
 
 }
