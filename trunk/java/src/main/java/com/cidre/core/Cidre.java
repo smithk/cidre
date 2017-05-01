1baf6761dfc240632450af883db93556f38ec667
Emil Rozbicki <emil@glencoesoftware.com>
2017-05-01 16:01:14 +0200

    CIDRE process

--- trunk/java/src/main/java/com/cidre/core/Cidre.java
+++ trunk/java/src/main/java/com/cidre/core/Cidre.java
@@ -1,6 +1,9 @@
 package com.cidre.core;
 
 import java.io.File;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +11,15 @@ import org.slf4j.LoggerFactory;
 import com.cidre.algorithms.CidreMath;
 import com.cidre.io.BfImageLoader;
 import com.cidre.io.BfImageWriter;
+import com.cidre.io.BfModelLoader;
 import com.cidre.io.BfModelWriter;
 
+import loci.common.services.ServiceFactory;
 import loci.formats.FormatTools;
+import loci.formats.MetadataTools;
+import loci.formats.meta.IMetadata;
+import loci.formats.out.TiffWriter;
+import loci.formats.services.OMEXMLService;
 
 public class Cidre {
 
@@ -35,29 +44,96 @@ public class Cidre {
 
     private final double lambdaZDefaultValue = 0.5;
 
-    private ModelDescriptor descriptor = null;
+    private List<ModelDescriptor> descriptors = null;
 
-    private String input;
+    private String input = null;
 
-    private String outputDir;
+    private String modelInput = null;
 
-    private int width;
+    private String outputDir = null;
 
-    private int height;
+    private String modelOutputDir = null;
+
+    private BfImageLoader image_loader = null;
+
+    private BfImageLoader imageLoader = null;
+
+    private boolean useMinImage = false;
+
+    private boolean skipPreProcessing = false;
 
     public Cidre(String fileName, String outputDir) {
         this.input = fileName;
         this.outputDir = outputDir;
     };
 
+    public Cidre(String fileName, String outputDir,
+                 String modelInput, String modelOutputDir,
+                 boolean useMinImage, boolean skipPreprocessing)
+    {
+        this.input = fileName;
+        this.outputDir = outputDir;
+        this.modelInput = modelInput;
+        this.modelOutputDir = modelOutputDir;
+        this.useMinImage = useMinImage;
+        this.skipPreProcessing = skipPreprocessing;
+    }
+
+    public void setModelInput(String modelInput) {
+        this.modelInput = modelInput;
+    }
+
+    public void setModelOutputDir(String modelOutputDir) {
+        this.modelOutputDir = modelOutputDir;
+    }
+
+    public void setOutputDir(String outputDir) {
+        this.outputDir = outputDir;
+    }
+
+    public void setUseMinImage(boolean useMinImage) {
+        this.useMinImage = useMinImage;
+    }
+
+    public void setSkipPreprocessing(boolean skipPreprocessing) {
+        this.skipPreProcessing = skipPreprocessing;
+    }
+
+    public void execute() throws Exception {
+        if (this.input == null) {
+            throw new Exception("No input or empty.");
+        }
+        Options options = new Options();
+        this.imageLoader = new BfImageLoader(options, this.input);
+        if (this.modelInput == null) {
+            log.info("Building model");
+            this.buildModel();
+        } else {
+            log.info("Loading model");
+            this.loadModel();
+        }
+        if (this.modelOutputDir != null) {
+            log.info("Saving model files");
+            this.saveModel();
+        }
+        if (this.outputDir != null) {
+            log.info("Correcting files");
+            this.applyModel();
+        }
+    }
     /**
      * Build CIDRE model
      */
     public void buildModel()
     {
         log.info("Building CIDRE model");
+        if (this.descriptors == null) {
+            this.descriptors = new ArrayList<ModelDescriptor>();
+        } else {
+            this.descriptors.clear();
+        }
         Options options = new Options();
-        BfImageLoader image_loader = new BfImageLoader(options, this.input);
+        this.image_loader = new BfImageLoader(options, this.input);
         try {
             image_loader.initialise();
         } catch (Exception e) {
@@ -70,51 +146,115 @@ public class Cidre {
         log.info("Building model from {} images [{}, {}]",
                  image_loader.getSizeS(), image_loader.getWidth(),
                  image_loader.getHeight());
-        try {
-            image_loader.loadImages(0);
-        } catch (Exception e) {
-            log.error(e.toString());
-            e.printStackTrace();
-            return;
-        }
-        double[] zLimits = CidreMath.zLimitsFromPercentiles(
+        for (int channel = 0;
+             channel < this.image_loader.getSizeC(); channel++)
+        {
+            try {
+                image_loader.loadImages(channel);
+            } catch (Exception e) {
+                log.error(e.toString());
+                e.printStackTrace();
+                return;
+            }
+            double[] zLimits = CidreMath.zLimitsFromPercentiles(
                 image_loader.getMinImage());
-        options.zLimits[0] = zLimits[0];
-        options.zLimits[1] = zLimits[1];
-        ModelGenerator model = new ModelGenerator(options);
-        this.descriptor = model.generate(image_loader.getStack());
+            options.zLimits[0] = zLimits[0];
+            options.zLimits[1] = zLimits[1];
+            ModelGenerator model = new ModelGenerator(options);
+            ModelDescriptor descriptor = model.generate(
+                this.image_loader.getStack());
+            double[][] minImage = image_loader.getMinImage();
+            double[] buffer = new double[
+                image_loader.getHeight() * image_loader.getWidth()];
+            int width = this.image_loader.getWidth();
+            int height = this.image_loader.getHeight();
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    buffer[x * height + y] = minImage[x][y];
+                }
+            }
+            descriptor.minImage = buffer;
+            this.descriptors.add(descriptor);
+        }
     }
 
     /**
      * Load CIDRE model
+     * @throws Exception 
      */
-    public void loadModel() {};
+    public void loadModel() throws Exception {
+        BfModelLoader loader = new BfModelLoader(this.modelPath);
+        List<ModelDescriptor> descriptor = loader.loadModel();
+    };
 
     /**
      * Save CIDRE model
      */
     public void saveModel() throws Exception
     {
-        if (this.descriptor != null) {
-            this.saveModel(this.descriptor);
+        if (this.descriptors != null) {
+            this.saveModel(this.descriptors);
         } else {
             log.error("Could not save model Descriptor is Null");
         }
     }
 
-    public void saveModel(ModelDescriptor descriptor) throws Exception
+    public void saveModel(List<ModelDescriptor> descriptors2) throws Exception
     {
         log.info("Saving model to {}", this.outputDir);
-        String fileName = this.outputDir + File.separator
-                        + "Cidre_model.ome.tif";
-        BfModelWriter writer = new BfModelWriter(fileName, descriptor);
+        File f = new File(this.input);
+        String fileName = f.getName().split("\\.")[0] + ".ill.cor.ome.tif";
+        fileName = this.outputDir + File.separator + fileName;
+        BfModelWriter writer = new BfModelWriter(fileName, descriptors2);
         writer.saveModel();
     };
 
     /**
      * Apply model
+     * @throws Exception 
      */
-    public void applyModel() {};
+    public void applyModel() throws Exception {
+        if (this.image_loader == null) {
+            Options options = new Options();
+            this.image_loader = new BfImageLoader(options, this.input);
+        }
+        ServiceFactory factory = new ServiceFactory();
+        OMEXMLService service = factory.getInstance(OMEXMLService.class);
+        IMetadata meta = service.createOMEXMLMetadata();
+        int channel = 0, timepoint = 0, zPlane = 0;
+        double[][] pixels;
+        float[][] pixelsFloat;
+        String fileName;
+        ImageCorrection corrector = new ImageCorrection(
+            this.descriptors.get(0),
+            Options.CorrectionMode.DYNAMIC_RANGE_CORRECTED,
+            this.outputDir, this.image_loader);
+        MetadataTools.populateMetadata(
+            meta, 0, null, false, "XYZCT",
+            FormatTools.getPixelTypeString(FormatTools.FLOAT),
+            this.image_loader.getWidth(), this.image_loader.getHeight(),
+            1, 1, 1, 1);
+        for (int s = 0; s < this.image_loader.getSizeS(); s++) {
+            pixels = this.image_loader.loadPlane(
+                s, channel, timepoint, zPlane);
+            pixelsFloat = corrector.correctPlane(pixels);
+            fileName = this.outputDir + File.separator
+                     + "Output_" + String.format("%03d", s) + ".tif";
+            TiffWriter writer = new TiffWriter();
+            writer.setMetadataRetrieve(meta);
+            writer.setId(fileName);
+            int width = this.image_loader.getWidth();
+            int height = this.image_loader.getHeight();
+            ByteBuffer buffer = ByteBuffer.allocate(4 * width * height);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    buffer.putFloat(pixelsFloat[x][y]);
+                }
+            }
+            writer.saveBytes(0, buffer.array());
+            writer.close();
+        }
+    };
 
     private void printOptions(Options options){
         log.info(
